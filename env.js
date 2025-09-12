182fcd267cdd47f501eed2e02c6e6358d5d41be0
testuserdown <exefiledev@gmail.com>
2025-09-12 14:47:07 +0300

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.__ENV = {
-    SITE_TITLE: "Legends",
+    SITE_TITLE: "Anime Virtual Friends",
     X_HANDLE: "https://x.com/new",
     SITE_DESC: "A gentle app to reflect your mood with soothing anime vibes"
 };
